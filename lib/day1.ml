7f586948f406ea14b702a0017839aeee2058a89e
Simon Bundgaard-Egeberg <simon@bundgaard-egeberg.dk>
2023-06-11 09:11:11 +0200

    day 2 done

--- lib/day1.ml
+++ lib/day1.ml
@@ -43,4 +43,6 @@ let example =
   "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {|24000|}]
+let%expect_test _ = run example ; [%expect {|
+24000
+45000|}]
--- lib/day2.ml
+++ lib/day2.ml
@@ -1,24 +1,99 @@
 open! Imports
+open Base
 
 module M = struct
   (* Type to parse the input into *)
-  type t = unit
+
+  type rps = Rock | Paper | Scissor
+
+  type outcome = Win of rps | Lose of rps | Draw of rps
+
+  (* Parse a line of input into a type t *)
+  let char_to_rps = function
+    | 'A' | 'X' -> Rock
+    | 'B' | 'Y' -> Paper
+    | 'C' | 'Z' -> Scissor
+    | _ -> failwith "Invalid rpc"
+
+  let line_to_tuple str =
+    let a = String.split_on_chars str ~on:[' '] in
+    (char_to_rps (List.nth_exn a 1).[0], char_to_rps (List.nth_exn a 0).[0])
+
+  let rps_to_string = function
+    | Rock -> "Rock"
+    | Paper -> "Paper"
+    | Scissor -> "Scissor"
+
+  let rps_to_points = function Rock -> 1 | Paper -> 2 | Scissor -> 3
+
+  let outcome_to_points = function
+    | Win a -> 6 + rps_to_points a
+    | Lose a -> rps_to_points a
+    | Draw a -> 3 + rps_to_points a
+
+  (* let outcome_to_string = function *)
+  (*   | Win a -> *)
+  (*       "Win " ^ rps_to_string a ^ " " ^ Int.to_string *)
+  (*       @@ outcome_to_points (Win a) *)
+  (*   | Lose a -> *)
+  (*       "Lose " ^ rps_to_string a ^ " " ^ Int.to_string *)
+  (*       @@ outcome_to_points (Lose a) *)
+  (*   | Draw a -> *)
+  (*       "Draw " ^ rps_to_string a ^ " " ^ Int.to_string *)
+  (*       @@ outcome_to_points (Draw a) *)
+
+  let tuple_to_outcome (a, b) =
+    match (a, b) with
+    | Rock, Scissor | Scissor, Paper | Paper, Rock -> Win a
+    | Rock, Paper | Scissor, Rock | Paper, Scissor -> Lose a
+    | _ -> Draw a
+
+  type t = string list
 
   (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse inputs = Str.split (Str.regexp "\n") inputs
 
-  (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let part1 inputs =
+    let inputs =
+      inputs |> List.map ~f:line_to_tuple |> List.map ~f:tuple_to_outcome
+    in
+    let points = List.map ~f:outcome_to_points inputs in
+    let tp = List.fold_left points ~init:0 ~f:( + ) in
+    Stdio.printf "%d\n" tp
+
+  let list_to_tuple (l : string list) =
+    match l with [a; b] -> (a.[0], b.[0]) | _ -> failwith "Invalid input"
+
+  let line_to_points str =
+    let a = String.split_on_chars str ~on:[' '] in
+    (* X=lose, Y=draw Z=win *)
+    match list_to_tuple a with
+    | 'A', 'X' -> 3
+    | 'A', 'Y' -> 4
+    | 'A', 'Z' -> 8
+    | 'B', 'X' -> 1
+    | 'B', 'Y' -> 5
+    | 'B', 'Z' -> 9
+    | 'C', 'X' -> 2
+    | 'C', 'Y' -> 6
+    | 'C', 'Z' -> 7
+    | _ -> failwith "Invalid input"
 
   (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part2 inputs =
+    let points = List.map ~f:line_to_points inputs in
+    let tp = List.fold_left points ~init:0 ~f:( + ) in
+    Stdio.printf "%d\n" tp
 end
 
 include M
 include Day.Make (M)
 
-(* Example input *)
-let example = ""
+let example = "A Y\nB X\nC Z\n"
+
+(* Example of how to run day1 *)
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {|
+15
+12|}]
